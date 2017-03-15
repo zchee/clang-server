dc21c7c7db1c9812c529756778ff7e9ac45b47bd
Koichi Shiraishi <zchee.io@gmail.com>
2017-03-16 03:28:24 +0900

    indexdb: add godoc comment
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- indexdb/indexdb.go
+++ indexdb/indexdb.go
@@ -12,15 +12,17 @@ import (
 	"github.com/zchee/clang-server/internal/pathutil"
 )
 
-const (
-	index = "index.db"
-)
+// index filename of leveldb which indexed C/C++ symbols.
+const index = "index.db"
 
+// IndexDB represets a C/C++ file database using leveldb.
 type IndexDB struct {
 	root string
 	db   *leveldb.DB
 }
 
+// NewIndexDB creates the project cache directory, open the leveldb db file
+// on same location and return the new IndexDB.
 func NewIndexDB(root string) (*IndexDB, error) {
 	dir := pathutil.ProjectCacheDir(root)
 	db, err := leveldb.OpenFile(filepath.Join(dir, index), nil)
@@ -34,20 +36,27 @@ func NewIndexDB(root string) (*IndexDB, error) {
 	}, nil
 }
 
+// Close closes the leveldb.
 func (i *IndexDB) Close() error {
 	return i.db.Close()
 }
 
+// Put puts the selialized C/C++ files symbol data to leveldb.
+// The key is using blake2b hashed filename.
 func (i *IndexDB) Put(filename string, data []byte) error {
 	fileID := hashutil.NewHashString(filename)
 	return i.db.Put(hashutil.Encode(fileID), data, nil)
 }
 
+// Get gets the selialized C/C++ files symbol data from leveldb.
+// The key is using blake2b hashed filename.
 func (i *IndexDB) Get(filename string) ([]byte, error) {
 	fileID := hashutil.NewHashString(filename)
 	return i.db.Get(hashutil.Encode(fileID), nil)
 }
 
+// Has reports whether filename symbol data on leveldb.
+// The key is using blake2b hashed filename.
 func (i *IndexDB) Has(filename string) bool {
 	fileID := hashutil.NewHashString(filename)
 	has, err := i.db.Has(hashutil.Encode(fileID), nil)
