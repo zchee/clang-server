ae14b3109cec80ef511c9fd472053bfa6c33960e
Koichi Shiraishi <zchee.io@gmail.com>
2017-03-15 20:49:16 +0900

    vendor: switch filepathutil to osutil
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

---
+++ vendor/github.com/pkgutil/osutil/doc.go
@@ -0,0 +1,6 @@
+// Copyright 2017 The pkgutil Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package osutil provides the operating system-specific path information utility functions.
+package osutil
--- vendor/github.com/pkgutil/osutil/osutil.go
+++ vendor/github.com/pkgutil/osutil/osutil.go
@@ -2,33 +2,33 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package filepathutil
+package osutil
 
 import (
 	"os"
 )
 
-// IsExist returns whether the filename is exists.
+// IsExist reports whether the filename is exists.
 func IsExist(filename string) bool {
 	_, err := os.Stat(filename)
-	return err == nil && !os.IsNotExist(err)
+	return err == nil
 }
 
-// IsNotExist returns whether the filename is not exists.
+// IsNotExist reports whether the filename is not exists.
 func IsNotExist(filename string) bool {
 	_, err := os.Stat(filename)
 	return err != nil && os.IsNotExist(err)
 }
 
-// IsDir returns whether the filename is directory.
-func IsDir(filename string) bool {
-	fi, err := os.Stat(filename)
+// IsDirExist reports whether dir exists and which is directory.
+func IsDirExist(dir string) bool {
+	fi, err := os.Stat(dir)
 	return err == nil && fi.IsDir()
 }
 
-// MkdirAll checks whether the exist dir directory. and create directory to dir filepath if not exist.
+// MkdirAll checks whether the exist dir directory and create directory to dir filepath if not exist.
 func MkdirAll(dir string, perm os.FileMode) error {
-	if IsNotExist(dir) {
+	if !IsDirExist(dir) {
 		if err := os.MkdirAll(dir, perm); err != nil {
 			return err
 		}
