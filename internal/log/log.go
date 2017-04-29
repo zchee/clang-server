de4c5ffd69a92aadc545623d5fb6e5e8203e5e1b
Koichi Shiraishi <zchee.io@gmail.com>
2017-04-29 20:20:39 +0900

    internal/log: add godoc comment
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- internal/log/log.go
+++ internal/log/log.go
@@ -16,51 +16,65 @@ var (
 	debugLogger = log.New(os.Stderr, "DEBUG: ", log.Lshortfile)
 )
 
+// Print calls Output to print to the standard logger.
+// Arguments are handled in the manner of fmt.Print.
 func Print(v ...interface{}) {
 	logger.Output(2, fmt.Sprint(v...))
 }
 
+// Printf calls Output to print to the standard logger.
+// Arguments are handled in the manner of fmt.Printf.
 func Printf(format string, v ...interface{}) {
 	logger.Output(2, fmt.Sprintf(format, v...))
 }
 
+// Println calls Output to print to the standard logger.
+// Arguments are handled in the manner of fmt.Println.
 func Println(v ...interface{}) {
 	logger.Output(2, fmt.Sprintln(v...))
 }
 
+// Fatal is equivalent to Print() followed by a call to os.Exit(1).
 func Fatal(v ...interface{}) {
 	logger.Output(2, "FATAL: "+fmt.Sprint(v...))
 	os.Exit(1)
 }
 
+// Fatalf is equivalent to Printf() followed by a call to os.Exit(1).
 func Fatalf(format string, v ...interface{}) {
 	logger.Output(2, "FATAL: "+fmt.Sprintf(format, v...))
 	os.Exit(1)
 }
 
+// Fatalln is equivalent to Println() followed by a call to os.Exit(1).
 func Fatalln(v ...interface{}) {
 	logger.Output(2, "FATAL: "+fmt.Sprintln(v...))
 	os.Exit(1)
 }
 
+// Panic is equivalent to Print() followed by a call to panic().
 func Panic(v ...interface{}) {
 	s := fmt.Sprint(v...)
 	logger.Output(2, "PANIC: "+s)
 	panic(s)
 }
 
+// Panicf is equivalent to Printf() followed by a call to panic().
 func Panicf(format string, v ...interface{}) {
 	s := fmt.Sprintf(format, v...)
 	logger.Output(2, "PANIC: "+s)
 	panic(s)
 }
 
+// Panicln is equivalent to Println() followed by a call to panic().
 func Panicln(v ...interface{}) {
 	s := fmt.Sprintln(v...)
 	logger.Output(2, "PANIC: "+s)
 	panic(s)
 }
 
+// Debug calls Output to print to the standard logger if debug is true.
+// Arguments are handled in the manner of fmt.Print.
 func Debug(v ...interface{}) {
 	if len(debug) == 0 {
 		return
@@ -68,6 +82,8 @@ func Debug(v ...interface{}) {
 	debugLogger.Output(2, fmt.Sprint(v...))
 }
 
+// Debugf calls Output to print to the standard logger if debug is true.
+// Arguments are handled in the manner of fmt.Printf.
 func Debugf(format string, v ...interface{}) {
 	if len(debug) == 0 {
 		return
@@ -75,6 +91,8 @@ func Debugf(format string, v ...interface{}) {
 	debugLogger.Output(2, fmt.Sprintf(format, v...))
 }
 
+// Debugln calls Output to print to the standard logger if debug is true.
+// Arguments are handled in the manner of fmt.Println.
 func Debugln(format string, v ...interface{}) {
 	if len(debug) == 0 {
 		return
